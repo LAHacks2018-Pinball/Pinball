298bbb2ea11b0b49dc24890a7087f0ccc0076e4f
Andre Zhu <andrez@uci.edu>
2018-03-31 03:49:29 -0700

    initial commit

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(10, 0, 0);
+        rb.AddForce(force * 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //print("Hit");
+    }
+}
---
+++ Assets/PlaneMeshEdit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMeshEdit : MonoBehaviour {
+
+    MeshFilter mf;
+    Mesh mesh;
+
+	// Use this for initialization
+	void Start () {
+        mf = GetComponent<MeshFilter>();
+        mesh = mf.mesh;
+        Vector3[] vertices = mesh.vertices;
+
+
+        //change the plane to be higher in the center to allow pinballs to roll to either side
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
