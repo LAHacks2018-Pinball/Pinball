967eac63bb5f5f05b10816abf61ac3cbfc17e537
Andre Zhu <andrez@uci.edu>
2018-03-31 06:19:17 -0700

    Basic flipper functionality

--- Assets/BallCollision.cs
+++ Assets/BallCollision.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class BallCollision : MonoBehaviour {
 
     Rigidbody rb;
+    float gravity = -1000.0F;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        Physics.gravity = new Vector3(0, gravity, 0);
         rb = GetComponent<Rigidbody>();
         Vector3 force = new Vector3(10, 0, 0);
-        rb.AddForce(force * 10f);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Flipper.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour {
+
+    float motor_force = 1000;
+    float target_velocity = 1000;
+    bool should_flip = false;
+    HingeJoint hinge;
+    KeyCode keycode;
+
+	// Use this for initialization
+	void Start () {
+        //Component check so I don't have to make 4 different files controlling the flippers
+        string name = ToString();
+        print(name);
+        if (name == "P1_Flipper_Left (Flipper)")
+        {
+            keycode = KeyCode.Z;
+        }
+        else if (name == "P1_Flipper_Right (Flipper)")
+        {
+            keycode = KeyCode.C;
+        }
+        else if (name == "P2_Flipper_Left (Flipper)")
+        {
+            keycode = KeyCode.J;
+        }
+        else if (name == "P2_Flipper_Right (Flipper)")
+        {
+            keycode = KeyCode.L;
+        }
+        else
+        {
+            //Error
+            print("Flipper ID Error");
+        }
+
+
+        hinge = GetComponent<HingeJoint>();
+        JointMotor motor = hinge.motor;
+        motor.force = motor_force;
+        motor.targetVelocity = target_velocity;
+        motor.freeSpin = false;
+        hinge.motor = motor;
+        hinge.useMotor = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //For testing on computer
+        if (Input.GetKeyDown(keycode))
+        {
+            flip(true);
+        }
+        if (Input.GetKeyUp(keycode))
+        {
+            flip(false);
+        }
+
+        hinge.useMotor = should_flip;
+	}
+
+    void flip(bool flip_flag)
+    {
+        should_flip = flip_flag;
+    }
+}
